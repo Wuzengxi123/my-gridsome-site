2aa6dbb2abfda4c9a06b7f98bebea42b8ff40a63
Noah Kobayashi <noah.004b@gmail.com>
2019-04-13 23:25:41 +0900

    Add Netlify CMS admin

--- gridsome.config.js
+++ gridsome.config.js
@@ -25,7 +25,13 @@ module.exports = {
           }
         }
       }
-    }
+    },
+    {
+      use: 'gridsome-plugin-netlify-cms',
+      options: {
+        publicPath: '/admin'
+      }
+    },
   ],
 
   transformers: {
